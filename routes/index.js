2ea72aa264951d85e66a50c98d6580c585b2b5cc
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2018-05-18 17:00:34 +0200

    Proteins view now using im-tables #3

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
 });
 
-router.get('/diseases', function(req, res, next) {
-  res.render('diseases', { title: 'Diseases' });
-});
-
 router.get('/proteins', function(req, res, next) {
   res.render('proteins', { title: 'Proteins' });
-});
-
-router.get('/goannotations', function(req, res, next) {
-  res.render('goannotations', { title: 'GO Annotations' });
 });
 
 router.get('/genes', function(req, res, next) {
