1f88877203e3793781cfcca27d3574ed49707098
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2018-05-20 16:01:28 +0200

    Adding Mocha and Chai test framework + Testing GET number of HumanMine genes #4

---
+++ test/statistics.js
@@ -0,0 +1,29 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Statistics', () => {
+/*
+  * Test the count of elements per class
+  */
+  describe('/GET count of HumanMine genes', () => {
+      it('it should GET the count of HumanMine genes', (done) => {
+        chai.request(server)
+            .get('/statistics/count/humanmine/Gene')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('number');
+                res.body.should.be.at.least(0);
+              done();
+            });
+      });
+  });
+
+});
