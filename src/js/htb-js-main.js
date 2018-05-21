d71e853d0d2aa86f0b234237b3312cdd26ba5194
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2018-05-21 20:35:15 +0200

    Prototyping with the sidebar #5

--- src/js/htb-js-main.js
+++ src/js/htb-js-main.js
@@ -1,3 +1,26 @@
+$.fn.strech_text = function(){
+    var elmt          = $(this),
+        cont_width    = elmt.width(),
+        txt           = elmt.html(),
+        one_line      = $('<span class="stretch_it">' + txt + '</span>'),
+        nb_char       = elmt.text().length,
+        spacing       = cont_width/nb_char,
+        txt_width;
+    
+    elmt.html(one_line);
+    txt_width = one_line.width();
+    
+    if (txt_width < cont_width){
+        var  char_width     = txt_width/nb_char,
+             ltr_spacing    = spacing - char_width + (spacing - char_width)/nb_char ; 
+  
+        one_line.css({'letter-spacing': ltr_spacing});
+    } else {
+        one_line.contents().unwrap();
+        elmt.addClass('justify');
+    }
+};
+
 (function($) {
   "use strict"; // Start of use strict
   // Configure tooltips for collapsed side navigation
@@ -73,4 +96,12 @@
 	$("#proteinsCardText").text(response + " Proteins");
   });
   //
+  
+  $(document).ready(function () {
+    $('.stretch').each(function(){
+        $(this).strech_text();
+    });
+});
+  
+  
 })(jQuery); // End of use strict
