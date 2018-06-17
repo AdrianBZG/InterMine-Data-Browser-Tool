ba0dfd1d7f7bb4b0053e70e2b2af768cb7d667bb
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2018-06-17 21:18:34 +0200

    Fetch pathway names route #11

--- routes/fetch.js
+++ routes/fetch.js
@@ -3,6 +3,34 @@ var imjs = require('imjs');
 var request = require('request');
 var router = express.Router();
 
+/* GET Pathway Names from HumanMine inside a class (parameter). */
+router.get('/pathways/humanmine/:classname', function(req, res, next) {
+    var className = req.params.classname;
+
+    if (className != "Protein" && className != "Gene") {
+        res.status(500).send('You need to specify a valid class: Protein, Gene');
+    }
+
+    var service = new imjs.Service({
+        root: 'http://www.humanmine.org/humanmine/service'
+    });
+
+    var query = {
+        "from": className,
+        "select": "pathways.name",
+        "orderBy": [{
+            "path": "pathways.name",
+            "direction": "ASC"
+        }]
+    };
+
+    service.rows(query).then(function(rows) {
+        res.json(rows);
+    });
+
+
+});
+
 /* GET Datasets Names from HumanMine inside a class (parameter). */
 router.get('/datasets/humanmine/:classname', function(req, res, next) {
     var className = req.params.classname;
