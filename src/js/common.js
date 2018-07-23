3e0c2bbf2880f9bc923388c2907c074041c4e35c
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2018-07-23 22:34:41 +0200

    Change HTTPS warning #64

--- src/js/common.js
+++ src/js/common.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
  */
 function checkForHTTPS() {
     if (window.location.protocol.includes("https")) {
-        $("#navbarResponsive").prepend("<div class='alert' id='httpsAlert'><span class='closebtn' id='closeHttpsMessage'>×</span>You are currently viewing the HTTPS website. Due to security limitations, we are unable to show results from HTTP-only InterMines. You may be able to see more results if you <a href='http://im-browser-prototype.herokuapp.com/'>reload this site</a> via HTTP.</div><br/>");
+        $("#navbarResponsive").prepend("<div class='alert' id='httpsAlert'><span class='closebtn' id='closeHttpsMessage'>×</span>You are currently viewing the HTTPS website. Due to security limitations, we are unable to show results from HTTP-only InterMines. You may be able to see more results if you <a href='http://im-browser-prototype.herokuapp.com/'>reload this site</a> via HTTP, and/or allow unsafe scripts to run.</div><br/>");
 
         $("#closeHttpsMessage").click(function() {
             $("#httpsAlert").hide();
