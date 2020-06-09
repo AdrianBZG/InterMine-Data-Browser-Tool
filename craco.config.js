f6294f9a81336324f04e4beb244ca09202940f33
John Mendez <john.manuel.mendez@gmail.com>
2020-06-09 17:33:43 -0400

    Install and configure linaria
    Story
    [ch389]

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,14 @@
-module.exports = {}
+const CracoLinariaPlugin = require('craco-linaria')
+const { whenDev } = require('@craco/craco')
+
+module.exports = {
+	plugins: [
+		{
+			plugin: CracoLinariaPlugin,
+			options: {
+				// use a shorter name when in production
+				displayName: whenDev(() => true, false),
+			},
+		},
+	],
+}
