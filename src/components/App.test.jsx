75a41229f12e83bdc6870ca14233c0e6663f9d6b
John Mendez <john.manuel.mendez@gmail.com>
2020-06-10 12:54:25 -0400

    Set up a11y testing
    Story
    
    [ch401]

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import React from 'react'
 
 import { App } from './App'
@@ -7,6 +8,12 @@ test('renders learn react link', () => {
 	const { getByText } = render(<App />)
 	const linkElement = getByText(/learn react/i)
 	expect(linkElement).toBeInTheDocument()
+})
+
+it('should demonstrate this matcher`s usage with react', async () => {
+	render(<App />, document.body)
+	const results = await axe(document.body)
+	expect(results).toHaveNoViolations()
 })
 
 test('runs a normal jest test', () => {
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+
+import { toHaveNoViolations } from 'jest-axe'
+
+// make globally available for all tests
+expect.extend(toHaveNoViolations)
