6f7b4018e14523b18fd432317e2b36a6a11c01d9
John Mendez <john.manuel.mendez@gmail.com>
2020-06-10 09:13:20 -0400

    Place linaria options in the config file
    Linaria recommends not setting babel options in the babel file.
    
    Story:
    [ch389]

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,9 @@
 const CracoLinariaPlugin = require('craco-linaria')
-const { whenDev } = require('@craco/craco')
 
 module.exports = {
 	plugins: [
 		{
 			plugin: CracoLinariaPlugin,
-			options: {
-				// use a shorter name when in production
-				displayName: whenDev(() => true, false),
-			},
 		},
 	],
 }
---
+++ linaria.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	// use a shorter name when in production
+	displayName: process.env.NODE_ENV !== 'production',
+}
