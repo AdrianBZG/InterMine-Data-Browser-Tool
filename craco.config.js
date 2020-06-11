f4b562fd11c77fbbb8e302c92e5e6f9c5086a186
John Mendez <john.manuel.mendez@gmail.com>
2020-06-10 23:53:51 -0400

    Add stylelint webpack loader during development
    Adding this loader will display warnings in the terminal to the developer.
    This way they don't have to wait until committing files to verify if
    the linting check will fail.
    
    Story:
    [ch488]

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,24 @@
 const CracoLinariaPlugin = require('craco-linaria')
-const { when } = require('@craco/craco')
+const { when, whenDev } = require('@craco/craco')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const path = require('path')
 
 module.exports = {
 	webpack: {
-		plugins: [...when(Boolean(process.env.ANALYZE), () => [new BundleAnalyzerPlugin()], [])],
+		plugins: [
+			...when(Boolean(process.env.ANALYZE), () => [new BundleAnalyzerPlugin()], []),
+			whenDev(
+				() =>
+					new StyleLintPlugin({
+						configBasedir: __dirname,
+						context: path.resolve(__dirname, 'src'),
+						files: ['**/*.jsx'],
+						// allow errors to still compile during development
+						emitWarning: true,
+					})
+			),
+		],
 	},
 	plugins: [
 		{
