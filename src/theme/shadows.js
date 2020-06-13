413eba655d6ecec0ff76e334dfb48ea0e9d871ae
John Mendez <john.manuel.mendez@gmail.com>
2020-06-13 10:03:59 -0400

    Add box shadows to design system
    Clubhouse Story ID
    [ch492]

---
+++ src/theme/shadows.js
@@ -0,0 +1,11 @@
+/**
+ * Since box shadows for card elevations are theme specific, we set them as css
+ * variables in the relevant themes and just reference here for IDE autocomplete.
+ */
+export const shadows = {
+	elevation0: 'var(--boxShadow-elevation-0)',
+	elevation1: 'var(--boxShadow-elevation-1)',
+	elevation2: 'var(--boxShadow-elevation-2)',
+	elevation3: 'var(--boxShadow-elevation-3)',
+	elevation4: 'var(--boxShadow-elevation-4)',
+}
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,6 +3,21 @@ import { css } from 'linaria'
 export const lightTheme = css`
 	-webkit-font-smoothing: auto;
 	-moz-osx-font-smoothing: auto;
+
+	--boxShadow-elevation-0: 0 0 0 1px rgba(16, 22, 26, 0.15), 0 0 0 rgba(16, 22, 26, 0),
+		0 0 0 rgba(16, 22, 26, 0);
+
+	--boxShadow-elevation-1: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 0 0 rgba(16, 22, 26, 0),
+		0 1px 1px rgba(16, 22, 26, 0.2);
+
+	--boxShadow-elevation-2: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 1px 1px rgba(16, 22, 26, 0.2),
+		0 2px 6px rgba(16, 22, 26, 0.2);
+
+	--boxShadow-elevation-3: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 2px 4px rgba(16, 22, 26, 0.2),
+		0 8px 24px rgba(16, 22, 26, 0.2);
+
+	--boxShadow-elevation-4: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 4px 8px rgba(16, 22, 26, 0.2),
+		0 18px 46px 6px rgba(16, 22, 26, 0.2);
 `
 
 export const darkTheme = css`
@@ -12,4 +27,19 @@ export const darkTheme = css`
       Setting it globally allows us to change it when light mode is on */}
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
+
+	--boxShadow-elevation-0: 0 0 0 1px rgba(16, 22, 26, 0.4), 0 0 0 rgba(16, 22, 26, 0),
+		0 0 0 rgba(16, 22, 26, 0);
+
+	--boxShadow-elevation-1: 0 0 0 1px rgba(16, 22, 26, 0.2), 0 0 0 rgba(16, 22, 26, 0),
+		0 1px 1px rgba(16, 22, 26, 0.4);
+
+	--boxShadow-elevation-2: 0 0 0 1px rgba(16, 22, 26, 0.2), 0 1px 1px rgba(16, 22, 26, 0.4),
+		0 2px 6px rgba(16, 22, 26, 0.4);
+
+	--boxShadow-elevation-3: 0 0 0 1px rgba(16, 22, 26, 0.2), 0 2px 4px rgba(16, 22, 26, 0.4),
+		0 8px 24px rgba(16, 22, 26, 0.4);
+
+	--boxShadow-elevation-4: 0 0 0 1px rgba(16, 22, 26, 0.2), 0 4px 8px rgba(16, 22, 26, 0.4),
+		0 18px 46px 6px rgba(16, 22, 26, 0.4);
 `
