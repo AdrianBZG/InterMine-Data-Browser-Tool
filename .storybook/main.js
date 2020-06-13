e283a5e087d9749091487512f4dd2b49c47192d1
John Mendez <john.manuel.mendez@gmail.com>
2020-06-13 19:53:35 -0400

    Configure linaria babel loader for storybook development
    Clubhouse Story ID
    [ch573]

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,4 +14,16 @@ module.exports = {
 		'../docs/getting-started/*.story.mdx',
 		'../src/**/*.story.jsx',
 	],
+	webpackFinal: async (config, { configType }) => {
+		config.module.rules.push({
+			test: /\.jsx?$/,
+			loader: 'linaria/loader',
+			options: {
+				sourceMap: false,
+				cacheDirectory: 'src/.linaria_cache/storybook',
+			},
+		})
+
+		return config
+	},
 }
