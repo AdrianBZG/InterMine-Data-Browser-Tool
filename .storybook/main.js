b5eaa2ad9c2e45b5d06f1f1a212a75cc324a5874
John Mendez <john.manuel.mendez@gmail.com>
2020-06-14 12:24:59 -0400

    Add Readme to storybook documentation
    Clubhouse Story ID
    [ch573]

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,7 @@ module.exports = {
 			},
 		},
 	],
-	stories: ['../docs/**/*.story.mdx', '../src/**/*.story.jsx'],
+	stories: ['../README.story.mdx', '../docs/**/*.story.mdx', '../src/**/*.story.jsx'],
 	webpackFinal: async (config, { configType }) => {
 		config.module.rules.push({
 			test: /\.jsx?$/,
