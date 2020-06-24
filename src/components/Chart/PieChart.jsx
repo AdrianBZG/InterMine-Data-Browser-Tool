852c47fd0a5a9f78e7cc37fbd9f3e9336a243d84
John Mendez <john.manuel.mendez@gmail.com>
2020-06-24 17:36:32 -0400

    Render pie chart using chartjs

--- src/components/Chart/PieChart.jsx
+++ src/components/Chart/PieChart.jsx
@@ -1,7 +1,9 @@
-import { ResponsivePie } from '@nivo/pie'
+import { schemePaired } from 'd3-scale-chromatic'
 import imjs from 'imjs'
 import { styled } from 'linaria/react'
+import pattern from 'patternomaly'
 import React, { useEffect, useState } from 'react'
+import { Doughnut } from 'react-chartjs-2'
 
 import { geneQueryStub, mineUrl } from '../../stubs/utils'
 
@@ -17,8 +19,10 @@ const S = {
 	Label,
 }
 
-export const PieChart = () => {
-	const [chartData, setChartData] = useState([])
+export const PieChart = ({ isColorBlind = false }) => {
+	const [chartData, setChartData] = useState({ data: [], labels: [] })
+	const colorPalette = isColorBlind ? pattern.generate(schemePaired) : schemePaired
+
 	const service = new imjs.Service({ root: mineUrl })
 	const query = new imjs.Query(geneQueryStub, service)
 
@@ -26,13 +30,15 @@ export const PieChart = () => {
 		const runQuery = async () => {
 			try {
 				const summary = await query.summarize('Gene.organism.shortName', 50)
-				setChartData(
-					summary.results.map((res) => ({
-						id: res.item,
-						value: res.count,
-						label: `${res.item} (${res.count})`,
-					}))
-				)
+				const data = []
+				const labels = []
+
+				summary.results.forEach((item) => {
+					data.push(item.count)
+					labels.push(item.item)
+				})
+
+				setChartData({ data, labels })
 			} catch (e) {
 				console.error(e.message)
 			}
@@ -45,81 +51,11 @@ export const PieChart = () => {
 
 	return (
 		<>
-			<ResponsivePie
-				data={chartData}
-				innerRadius={0.5}
-				padAngle={0.8}
-				margin={{ left: -200, top: 0, bottom: 0, right: 20 }}
-				cornerRadius={3}
-				colors={{ scheme: 'accent' }}
-				enableSlicesLabels={false}
-				enableRadialLabels={false}
-				borderWidth={1}
-				sortByValue={true}
-				borderColor={{ from: 'color', modifiers: [['darker', 0.5]] }}
-				animate={true}
-				motionStiffness={90}
-				motionDamping={15}
-				theme={{
-					legends: {
-						text: {
-							fontSize: 'var(--fs-desktopS2)',
-						},
-					},
+			<Doughnut
+				data={{
+					datasets: [{ data: chartData.data, backgroundColor: colorPalette }],
+					labels: chartData.labels,
 				}}
-				defs={[
-					{
-						id: 'dots',
-						type: 'patternDots',
-						background: 'inherit',
-						color: 'rgba(255, 255, 255, 0.3)',
-						size: 4,
-						padding: 1,
-						stagger: true,
-					},
-					{
-						id: 'lines',
-						type: 'patternLines',
-						background: 'inherit',
-						color: 'rgba(255, 255, 255, 0.3)',
-						rotation: -45,
-						lineWidth: 6,
-						spacing: 10,
-					},
-				]}
-				fill={[
-					{
-						match: (d) => {
-							return d.index % 5 === 0
-						},
-						id: 'dots',
-					},
-					{
-						match: (d) => {
-							return d.index % 3 === 0
-						},
-						id: 'lines',
-					},
-				]}
-				legends={[
-					{
-						anchor: 'top-right',
-						direction: 'column',
-						itemWidth: 200,
-						itemHeight: 36,
-						itemTextColor: 'var(--blue9)',
-						symbolSize: 24,
-						symbolShape: 'circle',
-						effects: [
-							{
-								on: 'hover',
-								style: {
-									itemTextColor: '#000',
-								},
-							},
-						],
-					},
-				]}
 			/>
 			<S.Label>Number of results for Gene by organism</S.Label>
 		</>
