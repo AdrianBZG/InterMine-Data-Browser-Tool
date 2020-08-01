77417301cc35e7c9fcce608aca1f571e4671c3f2
John Mendez <john.manuel.mendez@gmail.com>
2020-07-31 20:15:10 -0400

    Chore: fix pluralization

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ export const noop = () => {}
 export const formatConstraintPath = ({ classView, path }) => `${classView}.${path}`
 
 export const pluralizeFilteredCount = (filteredItems, query) => {
-	const isPlural = filteredItems.length > 1 ? 's' : ''
+	const isPlural = filteredItems.length === 1 ? '' : 's'
 
 	return query === ''
 		? `Showing ${filteredItems.length} Item${isPlural}`
