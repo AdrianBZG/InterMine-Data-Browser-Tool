4e02fb0ddfb8b06efa27d13aa4c7b32179e8c3e1
John Mendez <john.manuel.mendez@gmail.com>
2020-08-03 21:51:52 -0400

    Disable scrollbar on body element

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import 'normalize.css'
 import './theme/main.css'
 
 import { FocusStyleManager } from '@blueprintjs/core'
+import { css, Global } from '@emotion/core'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
@@ -10,7 +11,19 @@ import * as serviceWorker from './serviceWorker'
 
 FocusStyleManager.onlyShowFocusOnTabs()
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<>
+		<Global
+			styles={css`
+				body {
+					overflow: hidden;
+				}
+			`}
+		/>
+		<App />
+	</>,
+	document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
